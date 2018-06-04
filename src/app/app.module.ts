8d38c0852f6096663b1309ba337ba19dd3d33bd8
gardheamsantos <gardheam.santos@yahoo.com.br>
2018-06-04 11:22:50 -0300

    criacao components/html bottstraps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { TopoComponent} from './topo/topo.component';
+import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
-import { TentativasComponent } from './tentativas/tentativas.component'
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component'
+
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     PainelComponent,
-    TentativasComponent
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
