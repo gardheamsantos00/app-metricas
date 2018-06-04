1c6c52579dec20db9e4681aee574ac69088f2e22
gardheamsantos <gardheam.santos@yahoo.com.br>
2018-06-04 14:55:46 -0300

    decremetando os coracoes(tentativas)

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -23,7 +23,10 @@ export class TentativasComponent implements OnInit ,OnChanges{
   constructor() { }
 
   ngOnChanges(){
-    console.log(this.tentativas)
+    if(this.tentativas !== this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas
+      this.coracoes[indice - 1].cheio = false
+    }
   }
 
   ngOnInit() {
