cff979680b66b839e00dbcf6012c436cc10bb9b6
gardheamsantos <gardheam.santos@yahoo.com.br>
2018-06-04 11:45:56 -0300

    binding topo img

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TentativasComponent implements OnInit {
 
+  public coracaoVazio: string = '/assets/coracao_vazio.png'
+  public coracaoCheio: string = '/assets/coracao_cheio.png'
+  
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
